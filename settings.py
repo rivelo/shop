cd7da31342c5b81ed13c64319cb054d5a2171fdc
unknown <rivelo@ymail.com>
2018-01-20 12:05:44 +0200

    fixed input_date_format

--- settings.py
+++ settings.py
@@ -46,7 +46,29 @@ DATE_FORMAT = 'd/m/Y'
 DATETIME_FORMAT = 'D d M Y'
 SHORT_DATE_FORMAT = 'd/m/Y'
 SHORT_DATETIME_FORMAT = 'd/m/Y'
-DATE_INPUT_FORMATS = ('%d/%m/%Y')
+
+DATE_INPUT_FORMATS = (
+    '%d/%m/%Y',     # '25/10/2006'
+    '%d/%m/%Y %H:%M:%S',     # '25/10/2006 14:30:59'
+    '%Y-%m-%d %H:%M:%S',     # '2006-10-25 14:30:59'
+    '%Y-%m-%d %H:%M:%S.%f',  # '2006-10-25 14:30:59.000200'
+    '%Y-%m-%d %H:%M',        # '2006-10-25 14:30'
+    '%Y-%m-%d',              # '2006-10-25'
+    '%m/%d/%Y %H:%M:%S',     # '10/25/2006 14:30:59'
+    '%m/%d/%Y %H:%M:%S.%f',  # '10/25/2006 14:30:59.000200'
+    '%m/%d/%Y %H:%M',        # '10/25/2006 14:30'
+    '%m/%d/%Y',              # '10/25/2006'
+    '%m/%d/%y %H:%M:%S',     # '10/25/06 14:30:59'
+    '%m/%d/%y %H:%M:%S.%f',  # '10/25/06 14:30:59.000200'
+    '%m/%d/%y %H:%M',        # '10/25/06 14:30'
+    '%m/%d/%y',              # '10/25/06'
+)
+TIME_INPUT_FORMATS = (
+    '%H:%M:%S',     # '14:30:59'
+    '%H:%M:%S.%f',  # '14:30:59.000200'
+    '%H:%M',        # '14:30'
+)
+#DATETIME_INPUT_FORMATS = 'd/m/Y H:M:S'
 
 HTTP_MINI_SERVER_IP = '10.1.1.185' # work server
 #HTTP_MINI_SERVER_IP = '10.1.1.180' # work server
