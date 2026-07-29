7eaa59ea260487b11817c13e06b66b5dcbf3e213
unknown <rivelo@ymail.com>
2026-07-29 12:33:27 +0300

    update View Client Balance

--- urls.py
+++ urls.py
@@ -376,13 +376,13 @@ urlpatterns = [
     url(r'^clientdebts/add/(?P<id>\d+)/$', catalog.clientdebts_add, name="client-debet-add"),
     url(r'^clientdebts/view/$', catalog.clientdebts_list),
     url(r'^clientdebts/edit/(?P<id>\d+)/$', catalog.clientdebts_edit, name='client-debet-edit'),
-    url(r'^clientdebts/delete/(?P<id>\d+)$', catalog.clientdebts_delete),
+    url(r'^clientdebts/delete/(?P<id>\d+)$', catalog.clientdebts_delete, name='client-debet-delete'),
     url(r'^clientdebts/(?P<client_id>\d+)/delete/all/$', catalog.clientdebts_delete_all),
 
     url(r'^clientcredits/add/(?P<id>\d+)/$', catalog.clientcredits_add, name="client-credit-add"),
     url(r'^clientcredits/view/$', catalog.clientcredits_list),
     url(r'^clientcredits/edit/(?P<id>\d+)/$', catalog.clientcredits_edit, name='client-credit-edit'),    
-    url(r'^clientcredits/delete/(?P<id>\d+)/$', catalog.clientcredits_delete),
+    url(r'^clientcredits/delete/(?P<id>\d+)/$', catalog.clientcredits_delete, name='client-credit-delete'),
     url(r'^clientcredits/(?P<client_id>\d+)/delete/all/$', catalog.clientcredits_delete_all),
     url(r'^clientcredits/set/$', catalog.clientcredits_set),
 
